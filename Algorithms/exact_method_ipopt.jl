ad2a29465d54e12741495ad03c824080840f586e
scotthanton <scott_hanton94@hotmail.com>
2017-02-28 09:54:46 -0600

    Updated method, still not a valid Ipopt model though

--- Algorithms/exact_method_ipopt.jl
+++ Algorithms/exact_method_ipopt.jl
@@ -3,6 +3,8 @@ using AmplNLReader
 using NLPModels
 using ForwardDiff
 
+new_bounds = Array{Float64}
+
 #=
 Attempts to shrink the bounds on the given NLP by using IPOPT
 external solver
@@ -14,40 +16,112 @@ function run2(filename)
   m = AmplModel(filename)
 
 
-  #Create Ipopt problem from model
-  prob = createProblem(m.meta.nvar, m.meta.lvar, m.meta.uvar, m.meta.ncon,
+  #Create Ipopt problem from model to maximize
+  prob_max = createProblem(m.meta.nvar, m.meta.lvar, m.meta.uvar, m.meta.ncon,
                        m.meta.lcon, m.meta.ucon, m.meta.nnzj, m.meta.nnzh,
-                       eval_f, eval_g, eval_grad_f, eval_jac_g, eval_h)
+                       eval_f_max, eval_g, eval_grad_f, eval_jac_g, eval_h)
 
-  #Specify starting point
-  prob.x = [0.0, 0.0]
+  #Create Ipopt problem from model to minimize
+  prob_max = createProblem(m.meta.nvar, m.meta.lvar, m.meta.uvar, m.meta.ncon,
+                       m.meta.lcon, m.meta.ucon, m.meta.nnzj, m.meta.nnzh,
+                       eval_f_min, eval_g, eval_grad_f, eval_jac_g, eval_h)
 
   #Set index of current variable we are min/max'ignoring
   current_var_index = 1
 
-  #Solve the problem
-  status = solveProblem(prob)
+  #For each variable in the NLP
+  while current_var_index <= m.meta.nvar
 
-  #Print out result
-  #println(Ipopt.ApplicationReturnStatus[status])
-  println(prob.x)
-  println(prob.obj_val)
+    #Solve the max and min prob;em
+    status_max = solveProblem(prob_max)
+    status_min = solveProblem(prob_min)
+
+    #Assign minimum variable value to variable bounds
+    new_bounds[current_var_index, 1] = prob_min.obj_val
+
+    #Assign maximum variable value to variable bounds
+    new_bounds[current_var_index, 2] = prob_max.obj_val
+
+    #Move to next variable
+    current_var_index = current_var_index + 1
+
+  end
+
+  #Return the shrunken bounds
+  return new_bounds
 
 end
 
 #=
 Returns the value of the objective function at the current solution x
+Maximize x
 =#
-function eval_f(x)
+function eval_f_max(x)
   global current_var_index
 
-  #Return the variable we are maximizing or minimizing (right now this will just maximize)
+  #Return the current variable to maximize
   return x[current_var_index]
+end
+
+#=
+Returns the value of the objective function at the current solution x
+Minimize x
+=#
+function eval_f_min(x)
+  global current_var_index
+
+  #Return the current variable to minimize
+  return -x[current_var_index]
 end
 
 #=
 Sets the value of the constraint functions g at the current solution x:
 =#
+function eval_g(x, g)
+  global m
+
+  #Constraint counter for looping all constraints
+  constraint_counter = 1
+
+  #Loop each constraint
+  for c in cons(m, x)
+
+    #Assign value of constraint function
+    g[constraint_counter] = jth_congrad(m, x, constraint_counter)
+
+    #Increment constraint counter
+    constraint_counter = constraint_counter + 1
+
+  end
+
+end
+
+#=
+Sets the value of the gradient of the objective function at the current solution x:
+=#
+function eval_grad_f(x, grad_f)
+  global m
+
+  #Evaluate gradient of objective function at x
+  grad_f = grad(m, x)
+
+end
+
+#=
+Returns the Jacobian at x
+=#
+function eval_jac_g(x, mode, rows, cols, vals)
+  global m
+  return
+  rows, cols, vals = jac_coord(m, x)
+end
+
+function eval_h(x, mode, rows, cols, obj_factor, lambda, vals)
+  global m
+  rows, cols, vals = hprod(m, x, lamda, m.meta.y0, obj_factor)
+
+end
+
 function eval_g(x, g)
   global m
 
