bb5e024a2db6da5c0aa6c81cf0647040079e4f65
SarrankanPathmanathan <sarran_01@hotmail.ca>
2017-03-10 17:26:16 -0500

    Update AlgorithmTester.jl

--- DevTools/AlgorithmTester.jl
+++ DevTools/AlgorithmTester.jl
@@ -1,6 +1,8 @@
 #Experimental Tester Script for Algorithm
 
+include("GetANucleus.jl")
 include("NonLinearIntervalSampling.jl")
+
 
 BoundedModelPaths = ["/Models/Bounded/Level 1/TwoVariables/",
 "/Models/Bounded/Level 1/LessThan10VarsConstraints/",
@@ -12,7 +14,7 @@ UnboundedModelPaths = ["/Models/Unbounded/Level 1/TwoVariables/",
 "/Models/Unbounded/Level 2/11To50VarsConstraints/",
 "/Models/Unbounded/Level 2/100To500VarsConstraints/"]
 
-function AlgorithmTester(Algorithm::Function)
+function AlgorithmTesterBounded(Algorithm::Function)
   for path in BoundedModelPaths
     println(path)
     #Used to run a batch of test models
@@ -20,18 +22,24 @@ function AlgorithmTester(Algorithm::Function)
      for file in FileList
       print(file * "\n")
        #CHANGE ALGORITHM PARAMETERS HERE
-       Bounds = Algorithm(AmplModel(pwd()*path*file),((1*10)^20),(-(1*10)^20))
-     end
-  end
-  for path in UnboundedModelPaths
-    #Used to run a batch of test models
-    FileList = readdir(pwd()*path)
-     for file in FileList
-      print(file * "\n")
-       #CHANGE ALGORITHM PARAMETERS HERE
-       Bounds = Algorithm(AmplModel(pwd()*path*file),((1*10)^20),(-(1*10)^20))
+       Bounds = Algorithm(AmplModel(pwd()*path*file),(-(1*10)^20),((1*10)^20))
      end
   end
 end
 
-AlgorithmTester(NonLinearIntervalSampling)
+function AlgorithmTesterUnbounded(Algorithm::Function)
+  for path in UnboundedModelPaths
+  #Used to run a batch of test models
+  FileList = readdir(pwd()*path)
+   for file in FileList
+    println(path)
+    print(file * "\n")
+     #CHANGE ALGORITHM PARAMETERS HERE
+     Bounds = Algorithm(AmplModel(pwd()*path*file),(-(1*10)^20),((1*10)^20))
+   end
+ end
+end
+
+#AlgorithmTesterBounded(NonLinearIntervalSampling)
+#AlgorithmTesterUnbounded(NonLinearIntervalSampling)
+#AlgorithmTesterUnbounded(GetANucleus)
