245763bbd0493fb8f6dd68fbf93c9e099623250f
Hamza Arab <hamzaarab@cmail.carleton.ca>
2017-03-11 23:49:43 -0500

    Update ManualNonLinearRangeCutting.jl
    Updated algorithm

--- Algorithms/ManualNonLinearRangeCutting.jl
+++ Algorithms/ManualNonLinearRangeCutting.jl
@@ -1,3 +1,4 @@
+#http://www.sce.carleton.ca/faculty/chinneck/MProbe/MProbePaper2.pdf
 using NLPModels
 using AmplNLReader
 
@@ -11,8 +12,6 @@ numUnboundedL = -(1*10)^3
 #Prints the bounds at the beginning
 function PrintBounds(nvar,lBOUND,uBOUND)
 
-  println("Welcome to Manual Linear Range Cutting Algorithm!\n")
-  println("Beginning variable bounds\n")
   for i = 1:nvar
     lbound = lBOUND[i]
     if(lbound == -Inf)
@@ -29,66 +28,173 @@ function PrintBounds(nvar,lBOUND,uBOUND)
 end
 
 #Perform cut
-function Cut(number,lower,upper)
-  lex = [];
-  for i = 1:number
-    lex =  lower[i] + 100
-    convert(Int64, lex)
-    u = upper[i]
-    println("var" * string(i) * " -> [$lex,$u]\n")
+function Cut(number,lower, bound, index)
+  if(index == 1)
+    lex = [];
+    result = [];
+    lex =  lower[bound] + 100
+    lower[bound] = lower[bound] + 100
+    lex = round(Int, lex)
+    lex = lex - 100
+    push!(result, lower[bound])
+    push!(result, lower[bound] - 100)
+  # push!(result, lex - 100)
+  # println(result)
+    return result
+  elseif(index == 2)
+    lex = [];
+    result = [];
+    lex =  lower[bound+1] + 100
+    lower[bound+1] = lower[bound+1] + 100
+    lex = round(Int, lex)
+    lex = lex - 100
+    push!(result, lower[bound+1])
+    push!(result, lower[bound+1] - 100)
+  # push!(result, lex - 100)
+  # println(result)
+    return result
+  elseif(index == 3)
+    lex = [];
+    result = [];
+    lex =  upper[bound+1] - 100
+    upper[bound+1] = upper[bound] - 100
+    lex = round(Int, lex)
+    lex = lex + 100
+    push!(result, upper[bound])
+    push!(result, upper[bound] + 100)
+  # push!(result, lex - 100)
+  # println(result)
+    return result
+  elseif(index == 4)
+    lex = [];
+    result = [];
+    lex =  upper[bound+1] - 100
+    lower[bound+1] = upper[bound+1] - 100
+    lex = round(Int, lex)
+    lex = lex + 100
+    push!(result, upper[bound+1])
+    push!(result, upper[bound+1] + 100)
+  # push!(result, lex - 100)
+  # println(result)
+    return result
   end
-  return lex
+end
+
+function satisfiesInequalityConstraint(value,z,upper,lower)
+  return (value[z] >= lower[z] && value[z] <= upper[z])
+end
+
+function satisfiesEqualityLTConstraint(value,z,upper)
+  return (value[z] <= upper[z])
+end
+
+function satisfiesEqualityGTConstraint(value,z,lower)
+  return (value[z] >= lower[z])
+end
+
+#Method to check each point and verify that it is not satisfied
+function CheckConstraints(ncon, Points)
+
+  #For Each Constraint
+  for i = 1 : ncon
+  #Generate Sampling Points
+    # SamplingPoints = GenerateSamplingPoints(10,nvar,lvar,cut)
+    for p in Points
+    #Get the functional value
+      values = NLPModels.cons(model,p)
+    #Equality Constraint
+      if findfirst(econ,i) > 0
+        if (satisfiesEqualityLTConstraint(values,i,upper))
+          push!(LTEQ_feasiblePoints,p)
+        elseif (satisfiesEqualityGTConstraint(values,i,lower))
+          push!(GTEQ_feasiblePoints,p)
+        end
+    #Inequality Constraint
+      else
+        if(satisfiesInequalityConstraint(values,i,upper,lower))
+          push!(INEQ_feasiblePoints,p)
+        end
+      end
+    end
+  end
 end
 
 #Method that samples points
-function GenerateSamplingPoints(numOfPoints,nvar,lvar,uvar,cut)
+function GenerateSamplingPoints(numOfPoints,nvar,lvar,uvar)
   SamplingPoints = Any[]
   for i = 1: numOfPoints
     arr_Point = Float64[]
     for j = 1: nvar
-      point = rand(lvar[cut]:uvar[j])
+      point = rand(lvar[j]:uvar[j])
       push!(arr_Point,point)
     end
     push!(SamplingPoints,arr_Point)
   end
   # for k = 1: numOfPoints
-  #   p = SamplingPoints[k]
-  #   println("point" * string(k) * " -> $p\n")
-  # end
+    #  p = SamplingPoints[k]
+     # println("point" * string(k) * " -> $p\n")
+   # end
   return SamplingPoints
 end
-
 
 #function NonLinearRangeCutting(model)
    #Model info
 
-# x1 = 100.0
-
-# x2 = 100.0
-
-  #Starting point
-# x = [x1, x2]
-
   #Number of variables
-nvar = model.meta.nvar
+  nvar = model.meta.nvar
 
   #Lower Bound of all variables
-lvar = model.meta.lvar
+  lvar = model.meta.lvar
 
   #Upper Bound of all variables
-uvar = model.meta.uvar
+  uvar = model.meta.uvar
 
   #Lower Bound of all constraints
-lower = model.meta.lcon
+  lower = model.meta.lcon
 
   #Upper Bound of all constraints
-upper = model.meta.ucon
+  upper = model.meta.ucon
 
   #Equality Constraints
-econ = model.meta.jfix
+  econ = model.meta.jfix
 
-PrintBounds(nvar, lvar, uvar)
-# cut =
-cut = Cut(nvar, lvar, uvar)
-# println("The value of the cut is now " * string(cut) * "\n");
-GenerateSamplingPoints(10, nvar, lvar, uvar, cut)
+  #Number of Constraints
+  ncon = model.meta.ncon
+
+  LTEQ_feasiblePoints = Any[]
+  GTEQ_feasiblePoints = Any[]
+  INEQ_feasiblePoints = Any[]
+
+  Rounds = 12
+  b = 1
+# How to convert from Float to Int
+# x = convert(Int64, 1.0)
+
+  println("Welcome to Non Linear Range Cutting Algorithm!\n")
+  println("Beginning variable bounds\n")
+  PrintBounds(nvar, lvar, uvar)
+
+  for i = 1:Rounds
+    println("Round" * string(i) * "\n")
+    # a = lvar[1]
+    # b = lvar[2]
+    # c = uvar[1]
+    # d = uvar[2]
+    # println("Bounds: -> [$a,$b,$c,$d]\n")
+    cut = Cut(nvar, lvar, 1, b)
+  # println(cut)
+    PrintBounds(nvar, lvar, uvar)
+    Points = GenerateSamplingPoints(1000, nvar, lvar, cut)
+    CheckConstraints(ncon, Points)
+    # if(!(empty!(LTEQ_feasiblePoints)) || (empty!(GTEQ_feasiblePoints)) || (empty!(ITEQ_feasiblePoints)))
+    # println(LTEQ_feasiblePoints)
+    # println(GTEQ_feasiblePoints)
+    # if(empty!(INEQ_feasiblePoints))
+    # println(INEQ_feasiblePoints)
+    if (length(INEQ_feasiblePoints) != 0)
+      b = b + 1
+    end
+  # end
+end
+# empty!(a)
+# rand(lower_bound_int:upper_bound_int)
