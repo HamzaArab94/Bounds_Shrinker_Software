94a9dfe042a8503ef73ef98bb9d160210596bb11
scotthanton <scott_hanton94@hotmail.com>
2017-03-21 15:50:57 -0500

    Settings Menu

--- GUI/GUI_callbacks.jl
+++ GUI/GUI_callbacks.jl
@@ -43,7 +43,7 @@ end
 #Callback for when shrink bounds is clicked
 function shrink_bounds_clicked_callback(leaf, button)
   global selected_model, selected_algorithm, new_bounds
-
+  global s_alpha, s_beta, s_ccpoints, s_ccmaxit
 
   #if the model has been specified
   if selected_model != "" && selected_algorithm != ""
@@ -52,31 +52,39 @@ function shrink_bounds_clicked_callback(leaf, button)
     new_bounds = Array{Float64}(selected_model.meta.nvar, 2)
 
     if selected_algorithm == "Manual Range Cutting"
-        return
+
     elseif selected_algorithm == "Get a Nucleus"
 
+      #Specify settings for get a nucleus and run
       GetANucleus.set_unbounded_lower_value(unbounded_lower_value)
       GetANucleus.set_unbounded_upper_value(unbounded_upper_value)
       GetANucleus.run(selected_model, new_bounds)
 
     elseif selected_algorithm == "Nonlinear Range Cutting"
-      return
+
     elseif selected_algorithm == "Nonlinear Interval Sampling"
 
+      #Specify settings for nonlinear interval sampling and run
       NonLinearIntervalSampling.set_unbounded_lower_value(unbounded_lower_value)
       NonLinearIntervalSampling.set_unbounded_upper_value(unbounded_upper_value)
       NonLinearIntervalSampling.run(selected_model, new_bounds)
 
     elseif selected_algorithm == "Constraint Consensus"
 
+      #Specify settings for constraint consensus and run
       ConstraintConsensus.set_unbounded_lower_value(unbounded_lower_value)
       ConstraintConsensus.set_unbounded_upper_value(unbounded_upper_value)
+      ConstraintConsensus.set_alpha(s_alpha)
+      ConstraintConsensus.set_beta(s_beta)
+      ConstraintConsensus.set_points_to_generate(s_ccpoints)
+      ConstraintConsensus.set_max_iterations(s_ccmaxit)
       ConstraintConsensus.run(selected_model, new_bounds)
 
     elseif selected_algorithm == "Exact Solution"
-      return
+
     end
 
+    #Update the variable list after the algorithm has run
     update_variable_list(true)
 
 
@@ -100,12 +108,6 @@ function select_algorithm_clicked_callback(leaf, button)
 
   selected_algorithm = getproperty(leaf, :label, String)
   setproperty!(saf_name, :label, selected_algorithm)
-
-  #Hide all algorithm options
-  hide_all_algorithm_options()
-
-  #Show algorithm options for the selected algorithm
-  #show_selected_algorithm_options()
 
   #if the model is not empty, we are ready to shrink
   if selected_model != ""
@@ -185,43 +187,132 @@ function update_model_details()
 
 end
 
-# Hides all algorithm specific options
-function hide_all_algorithm_options()
-  global s_alpha_name, s_alpha_input, s_beta_name, s_beta_input, s_points_name, s_points_input, s_maxit_name, s_maxit_input
+#Open the algorithm settings window
+function settings_clicked_callback(leaf, button)
 
-  println("hiding")
-  setproperty!(s_alpha_name, :visible, false)
-  setproperty!(s_alpha_input, :visible, false)
-  setproperty!(s_beta_name, :visible, false)
-  setproperty!(s_beta_input, :visible, false)
-  setproperty!(s_points_name, :visible, false)
-  setproperty!(s_points_input, :visible, false)
-  setproperty!(s_maxit_name, :visible, false)
-  setproperty!(s_maxit_input, :visible, false)
+  global unbounded_lower_value, unbounded_upper_value, s_alpha, s_beta, s_ccpoints, s_ccmaxit
+  global s_ulv_input, s_uuv_input, s_alpha_input, s_beta_input, s_ccpoints_input, s_ccmaxit_input, settings_window
+
+  settings_window = Window("Algorithm Settings", 800, 800, true, true)
+
+  #Grid layout
+  settings_grid = Grid()
+
+  #Our frame for model information
+  f1 = Frame("General Settings")
+  setproperty!(f1, :margin, 15)
+
+  #Grid for general settings
+  g_md = Grid()
+  setproperty!(g_md, :margin, 10)
+  setproperty!(g_md, :column_spacing, 15)
+
+  #Settings for Constraint Consensus
+  s_ulv_name = Label("Unbounded Lower Value")
+  s_ulv_input = Entry()
+  setproperty!(s_ulv_input, :text, unbounded_lower_value)
+
+  s_uuv_name = Label("Unbounded Upper Value")
+  s_uuv_input = Entry()
+  setproperty!(s_uuv_input, :text, unbounded_upper_value)
+
+  g_md[1, 1] = s_ulv_name
+  g_md[2, 1] = s_ulv_input
+  g_md[1, 2] = s_uuv_name
+  g_md[2, 2] = s_uuv_input
+
+  push!(f1, g_md)
+
+  #Our frame for model information
+  f2 = Frame("Constraint Consensus")
+  setproperty!(f2, :margin, 15)
+
+  #Grid for general settings
+  g_md2 = Grid()
+  setproperty!(g_md2, :margin, 10)
+  setproperty!(g_md2, :column_spacing, 15)
+
+  #Settings for Constraint Consensus
+  s_alpha_name = Label("Alpha")
+  s_alpha_input = Entry()
+  setproperty!(s_alpha_input, :text, s_alpha)
+
+  s_beta_name = Label("Beta")
+  s_beta_input = Entry()
+  setproperty!(s_beta_input, :text, s_beta)
+
+  s_ccpoints_name = Label("Points to Generate")
+  s_ccpoints_input = Entry()
+  setproperty!(s_ccpoints_input, :text, s_ccpoints)
+
+  s_ccmaxit_name = Label("Max Iterations")
+  s_ccmaxit_input = Entry()
+  setproperty!(s_ccmaxit_input, :text, s_ccmaxit)
+
+  g_md2[1, 1] = s_alpha_name
+  g_md2[2, 1] = s_alpha_input
+  g_md2[1, 2] = s_beta_name
+  g_md2[2, 2] = s_beta_input
+  g_md2[1, 3] = s_ccpoints_name
+  g_md2[2, 3] = s_ccpoints_input
+  g_md2[1, 4] = s_ccmaxit_name
+  g_md2[2, 4] = s_ccmaxit_input
+
+  push!(f2, g_md2)
+
+  #Our frame for save or cancel buttons
+  f3 = Frame("Actions")
+  setproperty!(f3, :margin, 15)
+
+  #Grid for general settings
+  g_md3 = Grid()
+  setproperty!(g_md3, :margin, 10)
+  setproperty!(g_md3, :column_spacing, 15)
+
+  #Buttons
+  save_settings_btn = Button("Save")
+  id_save_settings= signal_connect(settings_btn_callback, save_settings_btn, "button-press-event")
+
+  cancel_settings_btn = Button("Cancel")
+  id_cancel_settings= signal_connect(settings_btn_callback, cancel_settings_btn, "button-press-event")
+
+  g_md3[1, 1] = save_settings_btn
+  g_md3[2, 1] = cancel_settings_btn
+
+  push!(f3, g_md3)
+
+  settings_grid[1, 1] = f1
+  settings_grid[1, 2] = f2
+  settings_grid[1, 3] = f3
+
+  #Push our grid items to main window
+  push!(settings_window, settings_grid)
+  showall(settings_window)
 
 end
 
-function show_selected_algorithm_options()
-  global g_sa, f3, selected_algorithm, s_alpha_name, s_alpha_input, s_beta_name, s_beta_input, s_points_name, s_points_input, s_maxit_name, s_maxit_input
+#Callback for when a button on the settings page is clicked
+function settings_btn_callback(leaf, button)
+  global unbounded_lower_value, unbounded_upper_value, s_alpha, s_beta, s_ccpoints, s_ccmaxit
+  global s_ulv_input, s_uuv_input, s_alpha_input, s_beta_input, s_ccpoints_input, s_ccmaxit_input, settings_window
 
-  if selected_algorithm == "Constraint Consensus"
+  #Get which button was clicked
+  button_clicked = getproperty(leaf, :label, String)
 
-    #Constraint consensus option locations
-    g_sa[1, 2] = s_points_name
-    g_sa[2, 2] = s_points_input
+  #If Save button clicked
+  if button_clicked == "Save"
 
-    g_sa[1, 3] = s_maxit_name
-    g_sa[2, 3] = s_maxit_input
+    unbounded_lower_value = parse(Int64, getproperty(s_ulv_input, :text, String))
+    unbounded_upper_value = parse(Int64, getproperty(s_uuv_input, :text, String))
 
-    g_sa[1, 4] = s_alpha_name
-    g_sa[2, 4] = s_alpha_input
-
-    g_sa[1, 5] = s_beta_name
-    g_sa[2, 5] = s_beta_input
-
-    g_sa[1, 6] = shrink_bounds_btn
-    g_sa[2, 6] = accept_bounds_btn
+    s_alpha = parse(Float64, getproperty(s_alpha_input, :text, String))
+    s_beta = parse(Float64, getproperty(s_beta_input, :text, String))
+    s_ccmaxit = parse(Int64, getproperty(s_ccmaxit_input, :text, String))
+    s_ccpoints = parse(Int64, getproperty(s_ccpoints_input, :text, String))
 
   end
+
+  #Close the window
+  destroy(settings_window)
 
 end
